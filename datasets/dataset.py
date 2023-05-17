0ae9289fc3753147f96e5cf30bd45f2d990b830f
jisuAn <ajs7270@snu.ac.kr>
2023-05-17 16:17:41 +0900

    Modify SVAMP dataset

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -12,36 +12,27 @@ BASE_PATH = Path(__file__).parent.parent
 
 @dataclass
 class Problem:
+    passage: str
     question: str
     answer: str
 
 class Dataset(data.Dataset):
     def __init__(self,
-                 data_path: Path = Path("data/processed/svamp/dev.json"),
+                 data_path: Path = Path("data/SVAMP.json"),
                  ):
         with open(Path(BASE_PATH, data_path), 'r') as f:
             self.orig_dataset = json.load(f)
 
         self.data = []
         for problem_dict in tqdm(self.orig_dataset, desc="Converting Problem to Features "):
-            question = problem_dict['question']
-            numbers = problem_dict['numbers']
-            answer = float(problem_dict['answer'])
+            passage = problem_dict['Body']
+            question = problem_dict['Question']
+            answer = float(problem_dict['Answer'])
 
             if float.is_integer(answer):
                 answer = int(answer)
 
-            # Replace numbers with a placeholder
-            # question = 'If Bryan has number0 books in each of his number1 bookshelves , how many books does he have in total ?'
-            # numbers = ['56.0', '9.0']
-            for i, number in enumerate(numbers):
-                number = float(number)
-                if float.is_integer(number):
-                    number = int(number)
-                question = question.replace(f'number{i}', str(number))
-
-            assert not re.match(f'number\d', question) , f"question: {question}"
-            problem = Problem(question=question, answer=answer)
+            problem = Problem(passage=passage, question=question, answer=answer)
             self.data.append(problem)
 
     def __getitem__(self, index) -> Problem:
@@ -49,7 +40,6 @@ class Dataset(data.Dataset):
 
     def __len__(self) -> int:
         return len(self.data)
-
 
 if __name__ == "__main__":
     dataset = Dataset()
--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-# Load SVAMP dataset
+from pathlib import Path
 
+from langchain.llms import LlamaCpp
+from langchain import PromptTemplate, LLMChain
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from datasets import Dataset
+
+# Load SVAMP dataset
+svamp = Dataset(Path("data/processed/svamp/dev.json"))
+
+# Load LlamaCpp
+llm = LlamaCpp()
